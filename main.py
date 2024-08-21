004b7c0142a5f977a8f814c5d5975f4ddfa12895
lucascastellani <lucas100castellani@gmail.com>
2024-08-21 16:06:12 -0300

    wip

--- main.py
+++ main.py
@@ -19,7 +19,18 @@ class NewsScraper:
         self.search_phrase = search_phrase
         self.category = category
         self.months = months
-        self.driver = webdriver.Firefox()
+
+        options = webdriver.FirefoxOptions()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument("--disable-extensions")
+        # options.add_argument("--disable-gpu")
+        options.add_argument('--disable-web-security')
+        options.add_argument("--start-maximized")
+        # options.add_argument('--remote-debugging-port=9222')
+        # options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+        self.driver = webdriver.Firefox(options=options)
         self.output_dir = "output"
         if not os.path.exists(self.output_dir):
             os.makedirs(self.output_dir)
